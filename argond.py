4b4bc334c4db564d3fccfb0510431c1062aa3c41
KrioB <krzysiek.b92@gmail.com>
2022-08-18 23:03:22 +0200

    Add logging, add empty loop, code cleanup

--- argond.py
+++ argond.py
@@ -1,64 +1,63 @@
 #!/usr/bin/python3
 
+# -------------------------------------------
+# Argon One Case main deamon script
+# ver 1.0
+# -------------------------------------------
+
 import smbus
 import RPi.GPIO
-#import os
 import time
 
-
+# Load configuration file
+# Uses global ConfigurationFile
+# Returns valid configuration
 def loadConfiguration():
 
+    # Default configuration
     defCurve = [[0, 100]]
 
     configuration = {
-        "time": 5,
+        "time": 10,
         "curve": []
     }
 
     try:
+        # Try to open configuration file
         confFile = open(ConfigurationFile, "r")
     except:
+        # Return default configuration
         configuration["curve"] = defCurve
         return configuration
 
+    # Read configuration file line by line
     for line in confFile.readlines():
 
+        # Trim white characters
         tmpLine = line.strip()
         if not tmpLine:
+            # Skip empty lines
             continue
         if tmpLine[0] == "#":
+            # Skip comments
             continue
         pair = tmpLine.split("=")
         if len(pair) != 2:
+            # Skip wrong lines
             continue
 
         k = pair[0]
         v = pair[1]
 
-        try:
-            k = int(round(float(k)))
+        if k.isdigit() and v.isdigit():
+            # Get (temperature, speed) point for curve property
+            k = int(k)
             if k < 0:
                 k = 0
             elif k > 100:
                 k = 100
 
-        except ValueError:
-            if not k in configuration:
-                continue
-
-        except:
-            continue
-
-        try:
-            v = int(round(float(v)))
-
-        except:
-            continue
-
-        if k in configuration:
-            configuration[k] = v
-
-        else:
+            v = int(v)
             if v <= 0:
                 v = 0
             elif v <= 25:
@@ -66,13 +65,31 @@ def loadConfiguration():
             elif v >= 100:
                 v = 100
 
+            # Push point to curve
             configuration["curve"].append([k, v])
+
+        elif k == "time" and v.isdigit():
+            # Get value for time property
+            v = int(v)
+            if v <= 0:
+                v = 0
+            elif v >= 100:
+                v = 100
+
+            configuration["time"] = v
+
+        else:
+            # Key is wrong
+            continue
 
     confFile.close()
 
+    # If curve has at least 2 points
     if len(configuration["curve"]) > 1:
+        # Sort curve
         configuration["curve"].sort(key=lambda x: x[0] * 1000 + x[1], reverse=False)
 
+        # Remove duplicates
         i = 1
         while i < len(configuration["curve"]):
             if configuration["curve"][i-1][0] == configuration["curve"][i][0]:
@@ -80,103 +97,160 @@ def loadConfiguration():
             else:
                 i = i + 1
 
+    # If curve has exactly 1 point
     if len(configuration["curve"]) == 1:
         if configuration["curve"][0][1] == 0:
+            # Set fan always off
             configuration["curve"][0][0] = 100
         else:
+            # Set fan always on
             configuration["curve"][0][0] = 0
 
+    # If curve has no points
     if len(configuration["curve"]) == 0:
+        # Default curve
         configuration["curve"] = defCurve
 
     return configuration
 
-
+# Get CPU temperature
+# Uses global TemperatureFile
+# Returns temperature in Celsius
 def getTemperature():
 
-    temperature = 100
+    # Default temperature
+    defTemperature = 100
 
     try:
+        # Try to open temperature file
         tFile = open(TemperatureFile, "r")
     except:
-        return temperature
+        # Return default temperature
+        return defTemperature
 
     try:
-        value = tFile.readline()
-        value = int(round(int(value)/1000))
+        temperature = tFile.readline()
+        # Convert mili Celsius to Celsius
+        temperature = int(round(int(temperature)/1000))
     except:
-        value = 100
+        temperature = defTemperature
 
     tFile.close()
-    temperature = value
 
     return temperature
 
-
+# Set fan speed
+# Uses global FanBus, FanAddress
+# Returns False or error message
 def setFanSpeed(speed):
 
-    FanBus.write_byte(FanAddress,speed)
+    errResponse = False
+    try:
+        FanBus.write_byte(FanAddress, speed)
+    except Exception as err:
+        errResponse = err
 
+    return errResponse
 
+# Save status in TMP
+# Uses global StatusFile
+# Returns False or error message
 def updateStatus(t, s):
+
+    errResponse = False
     try:
         status = open(StatusFile, "w+")
         status.write("fan={}\ntmp={}".format(s, t))
         status.close()
-    except:
-        pass
+    except Exception as err:
+        errResponse = err
 
+    return errResponse
+
+# Check board revision to use connected I2C bus
 revision = RPi.GPIO.RPI_REVISION
 if revision == 2 or revision == 3:
-	FanBus = smbus.SMBus(1)
+    busId = 1
 else:
-	FanBus = smbus.SMBus(0)
+    busId = 0
 
+print("I2C bus {}".format(busId))
+
+# Global variables
 FanAddress = 0x1a
-
+FanBus = smbus.SMBus(busId)
 StatusFile = "/tmp/argon-state"
 TemperatureFile = "/sys/class/thermal/thermal_zone0/temp"
 ConfigurationFile = "/etc/argond.conf"
 
+# Load configuration
 conf = loadConfiguration()
-
 steps = len(conf["curve"])
 
-
 if steps == 1:
-    setFanSpeed(conf["curve"][0][1])
-else:
+    # No curve, fan spins with constant speed
+    s = conf["curve"][0][1]
+    errFan = setFanSpeed(s)
+    if errFan:
+        print("Can not set fan\n{}".format(errFan))
+    else:
+        print("Fan speed set to {}%".format(s))
 
-    t0 = 0
-    p0 = 0
-
+    # Empty loop required
+    # systemd tries to relaunch this script every time it crashes or finishes
+    # this loop keeps process alive with minimum CPU work
     while True:
         t = getTemperature()
+        errStat = updateStatus(t, s)
+        if errStat:
+            print("Can not save status\n{}".format(errStat))
 
-        if t > t0:
-            s = conf["curve"][p0][1]
-            for p in range(p0, steps, 1):
-                if t < conf["curve"][p][0]:
-                    break
-                else:
-                    s = conf["curve"][p][1]
-                    p0 = p
+        time.sleep(3600)
 
-            setFanSpeed(s)
+t0 = 0
+p0 = 0
 
-        elif t < t0:
-            s = conf["curve"][p0][1]
-            for p in range(p0, -1, -1):
-                if t > conf["curve"][p][0]:
-                    break
-                else:
-                    s = conf["curve"][p][1]
-                    p0 = p
+# Main loop
+while True:
 
-            setFanSpeed(s)
+    t = getTemperature()
 
-        t0 = t
+    # CPU temperature is increasing
+    if t > t0:
+        p = p0
+        for point in range(p0, steps, 1):
+            if t < conf["curve"][point][0]:
+                break
+            else:
+                p = point
 
-        updateStatus(t, s)
+    # CPU temperature is decreasing
+    elif t < t0:
+        p = p0
+        for point in range(p0, -1, -1):
+            if t > conf["curve"][point][0]:
+                break
+            else:
+                p = point
 
-        time.sleep(conf["time"])
+    t0 = t
+
+    s = conf["curve"][p][1]
+
+    if p != p0:
+        # Fan speed changed
+        p0 = p
+
+        # Set fan
+        errFan = setFanSpeed(s)
+        if errFan:
+            print("Can not set fan\n{}".format(errFan))
+        else:
+            print("Fan speed set to {}%".format(s))
+
+    # Save status
+    errStat = updateStatus(t, s)
+    if errStat:
+        print("Can not save status\n{}".format(errStat))
+
+    time.sleep(conf["time"])
