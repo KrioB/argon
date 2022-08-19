d0f6508c92a911d0de68ccaf66aa8273f6305a4e
KrioB <krzysiek.b92@gmail.com>
2022-08-19 10:16:09 +0200

    Add logging to journal

--- argon-poweroff.py
+++ argon-poweroff.py
@@ -1,18 +1,45 @@
 #!/usr/bin/python3
 
+# -------------------------------------------
+# Argon One Case shutdown script
+# ver 1.0
+# -------------------------------------------
+
+import logging
+from systemd.journal import JournalHandler
 import sys
 import smbus
 import RPi.GPIO
 
+# Configure logger
+log = logging.getLogger(__name__)
+log.addHandler(JournalHandler())
+log.setLevel(logging.INFO)
+
+# Check board revision to use connected I2C bus
 revision = RPi.GPIO.RPI_REVISION
 if revision == 2 or revision == 3:
-	FanBus = smbus.SMBus(1)
+    busId = 1
 else:
-	FanBus = smbus.SMBus(0)
+    busId = 0
 
+log.info("I2C bus {}".format(busId))
+
+# Global variables
 FanAddress = 0x1a
+FanBus = smbus.SMBus(busId)
 
-FanBus.write_byte(FanAddress, 0)
+# Stop the fan
+try:
+    FanBus.write_byte(FanAddress, 0)
+    log.info("Fan stopped")
+except Exception as err:
+    log.error("Can not set fan\n{}".format(err))
 
+# Turn off power supply
 if len(sys.argv) > 1 and sys.argv[1] == "poweroff":
-    FanBus.write_byte(FanAddress, 0xFF)
+     try:
+        FanBus.write_byte(FanAddress, 0xFF)
+        log.info("Power off")
+    except Exception as err:
+        log.error("Can not power off\n{}".format(err))
